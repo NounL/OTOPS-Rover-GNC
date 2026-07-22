34437e899d6672cf1c3ed5ec8a0343b57c0251a7
Olly <olly.love@ontariotechu.net>
2026-07-22 00:20:29 -0400

    cleaning stuff

---
+++ cam_gui/base_cam_grid.py
@@ -0,0 +1,182 @@
+# Ares' Amazing Cameras (OTOPS 2026)
+# Olly Love
+# 2025/2026 Interface for controlling the OTOPS rover cameras at CIRC
+# Layouts/Design for each camera stream and its controls
+
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, Gdk
+
+gi.require_version("Gst", "1.0")
+from gi.repository import Gst
+
+# Grid Layout of a camera stream interface, with the stream, and control buttons
+# Consists of a main outer grid (Placeholder/stream, inner grid)
+# inner grid is a small menu for button placement
+class BaseCamGrid(Gtk.Grid):
+    def __init__(self, placement, pipeline_str, sink):
+        Gst.init(None)
+        Gtk.Grid.__init__(self)
+        self.placement = placement
+        # Stores string used to create gstreamer pipeline
+        self.pipeline_str = pipeline_str
+        self.pipeline = None
+        self.sink = sink
+        # Size of small stream, overwritten in child for large camera
+        self.width = 525
+        self.height = 295
+        
+        # Main grid everythings added to
+        self.main_cam_grid = Gtk.Grid()
+        self.add(self.main_cam_grid)
+        self.main_cam_grid.set_row_spacing(2)
+        self.main_cam_grid.set_column_spacing(2)
+        # Inner grid for on/off buttons and camera label 
+        self.inner_cam_grid = Gtk.Grid()
+        self.inner_cam_grid.set_row_spacing(2)
+        self.inner_cam_grid.set_column_spacing(5)
+
+        self.placeholder = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.set_screen_size()
+        self.placeholder.set_halign(Gtk.Align.START)
+        self.placeholder.set_valign(Gtk.Align.START)
+        # Background color is black
+        self.placeholder.override_background_color(
+            Gtk.StateFlags.NORMAL,
+            Gdk.RGBA(0, 0, 0, 1)
+        )
+
+        self.cam_lbl = Gtk.Label()
+        # Change text color
+        color = Gdk.RGBA()
+        color.parse("black")
+        self.cam_lbl.override_color(Gtk.StateFlags.NORMAL, color)
+        # Large text - can't seem to control font with numbers
+        self.cam_lbl.set_markup(f"<big>{self.placement}</big>")
+
+        # Create, connect, and add buttons to the display
+        self.on_btn = Gtk.Button(label="On")
+        self.off_btn = Gtk.Button(label="Off")
+
+        self.on_btn.connect("clicked", self.on_btn_click)
+        self.off_btn.connect("clicked", self.off_btn_click)
+
+        self.inner_cam_grid.attach(self.cam_lbl,0,0,1,1)
+        self.inner_cam_grid.attach(self.on_btn,1,0,1,1)
+        self.inner_cam_grid.attach(self.off_btn,2,0,1,1)
+        self._build_swaps()
+        self.main_cam_grid.attach(self.placeholder,0,0,1,1)
+        self.main_cam_grid.attach(self.inner_cam_grid,0,1,1,1)
+
+    def set_screen_size(self):
+        self.placeholder.set_size_request(self.width, self.height)
+
+    def stream_on(self):
+        # Only turn stream on when its off
+        if not self.pipeline:
+            self.pipeline = Gst.parse_launch(self.pipeline_str)
+            gtksink = self.pipeline.get_by_name(self.sink)
+            self.widgetSink = gtksink.props.widget
+            # same size as placeholder
+            self.widgetSink.set_size_request(self.width,self.height)
+            self.widgetSink.set_hexpand(False)
+            self.widgetSink.set_vexpand(False)
+
+            # Replacing empty placeholder with camera stream
+            self.main_cam_grid.remove(self.placeholder)
+            self.main_cam_grid.attach(self.widgetSink,0,0,1,1)
+            self.widgetSink.show()
+            self.pipeline.set_state(Gst.State.PLAYING)
+        else:
+            print("Error: Stream already on.")
+
+    def stream_off(self):
+        if self.pipeline:
+            self.pipeline.set_state(Gst.State.NULL)
+            self.pipeline = None
+
+            self.main_cam_grid.remove(self.widgetSink)
+
+            self.main_cam_grid.attach(self.placeholder,0,0,1,1)
+            self.placeholder.show()
+        else:
+            print("ERROR: Stream already disconnected.")
+
+    def on_btn_click(self, widget):
+        self.stream_on()
+
+    def off_btn_click(self, widget):
+        self.stream_off()
+        
+    # Overwritten by small cam child - basecam does nothing with this
+    def _build_swaps(self):
+        pass
+
+# This grid fits the large placeholder and has extra buttons to control swapping 
+# and resetting cameras 
+class LargeCamGrid(BaseCamGrid):
+    def __init__(self, placement, pipeline_str, sink, swap_callback):
+        super().__init__(placement, pipeline_str, sink)
+
+        self.swap_callback = swap_callback
+        self.width = 1280
+        self.height = 960
+        self.set_screen_size()
+        # For tracking swaps
+        self.left_shown = False
+        self.front_shown = False
+        self.right_shown = False
+        self.back_shown = False
+
+    def set_screen_size(self):
+        self.placeholder.set_size_request(self.width, self.height)
+
+    # Swap buttons to allow swapping camera streams
+    def _build_swaps(self):
+        self.show_left_btn = Gtk.Button(label="Show Left")
+        self.show_left_btn.connect("clicked", self.show_left_btn_click)
+        self.inner_cam_grid.attach(self.show_left_btn,3,0,1,1)
+
+        self.show_right_btn = Gtk.Button(label="Show Right")
+        self.show_right_btn.connect("clicked", self.show_right_btn_click)
+        self.inner_cam_grid.attach(self.show_right_btn,4,0,1,1)
+
+        self.show_back_btn = Gtk.Button(label="Show Back")
+        self.show_back_btn.connect("clicked", self.show_back_btn_click)
+        self.inner_cam_grid.attach(self.show_back_btn,5,0,1,1)
+
+        self.reset_front_btn = Gtk.Button(label="Reset Front")
+        self.reset_front_btn.connect("clicked", self.reset_front_btn_click)
+        self.inner_cam_grid.attach(self.reset_front_btn,6,0,1,1)
+
+    # Using a callback function in main window, sending keyword "Left"
+    # back to main window controlling swaps to swap based on the button pressed
+    def show_left_btn_click(self, widget):
+        self.front_shown = False
+        self.right_shown = False
+        self.back_shown = False
+        self.swap_callback("Left")
+        self.left_shown = True
+
+    def show_right_btn_click(self, widget):
+        self.front_shown = False
+        self.left_shown = False
+        self.back_shown = False
+        self.swap_callback("Right")
+        self.right_shown = True
+
+    def show_back_btn_click(self, widget):
+        self.front_shown = False
+        self.left_shown = False
+        self.right_shown = False
+        self.swap_callback("Back")
+        self.back_shown = True
+
+    def reset_front_btn_click(self, widget):
+        self.left_shown = False
+        self.right_shown = False
+        self.back_shown = False
+        self.swap_callback("Front")
+        self.front_shown = True
+        
+        
---
+++ cam_gui/main.py
@@ -0,0 +1,170 @@
+# Ares' Amazing Cameras (OTOPS 2026)
+# Olly Love
+# 2025/2026 Interface for controlling the OTOPS rover cameras at CIRC
+# Helpful Source: https://www.youtube.com/watch?v=FszqUfibD3w
+# Note: Server must be running for cameras to be able to connect to it
+# Make sure the IP addresses match that of the server
+
+# Installs: 
+# Make sure you have gstreamer installed through there webpage instructions
+# sudo apt install libgtk-3-dev gir1.2-gtk-3.0 python3-gi python3-gi-cairo
+# sudo apt install gstreamer1.0-gtk3
+
+# If face issues on groundstation, run like this:
+# GDK_BACKEND=x11 python3 gui_v2.py
+
+# How to run stream when developing - will later be just ./rtspstream
+# gcc latest-rtspstream.c $(pkg-config --cflags --libs gstreamer-rtsp-server-1.0)
+# ./a.out
+
+# Debugging purposes:
+# Scan for camera specs: 
+# v4l2-ctl --list-devices
+# v4l2-ctl --list-formats-ext --device /dev/video0
+
+# Testing cameras out of gui
+# gst-launch-1.0 rtspsrc location=rtsp://192.168.1.31:8554/front latency=0 drop-on-latency=true ! rtph264depay ! queue ! h264parse ! avdec_h264 ! videoconvert ! ximagesink
+# gst-launch-1.0 rtspsrc location=rtsp://192.168.1.31:8554/left latency=0 drop-on-latency=0 drop-on-latency=true ! rtpjpegdepay ! queue ! jpegdec ! videoconvert ! ximagesink
+# gst-launch-1.0 rtspsrc location=rtsp://192.168.1.31:8554/right latency=0 drop-on-latency=0 drop-on-latency=true ! rtpjpegdepay ! queue ! jpegdec ! videoconvert ! ximagesink
+# gst-launch-1.0 rtspsrc location=rtsp://192.168.1.31:8554/back latency=0 drop-on-latency=0 drop-on-latency=true ! rtpjpegdepay ! queue ! jpegdec ! videoconvert ! ximagesink
+
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, Gdk
+from base_cam_grid import BaseCamGrid, LargeCamGrid
+
+# Main window of the software, displays everything in a root container
+# Controls swap functionality
+class MainWindow(Gtk.Window):
+    def __init__(self):
+        # Super classes constructor
+        Gtk.Window.__init__(self, title="Ares' Amazing Cameras (OTOPS 2026)")
+        # Size to perfectly fit groundstation
+        self.set_default_size(1850,1000)
+        # Light gray background
+        self.override_background_color(
+            Gtk.StateFlags.NORMAL,
+            Gdk.RGBA(0.5, 0.5, 0.5, 1)
+        )
+
+        # Main root container which holds everything
+        root_grid = Gtk.Grid()
+        self.add(root_grid)
+
+        # Allign in the center and padding between items
+        root_grid.set_halign(Gtk.Align.CENTER)
+        root_grid.set_valign(Gtk.Align.CENTER)
+        root_grid.set_row_spacing(2)
+        root_grid.set_column_spacing(2)
+
+        # 1 H264 webcam, 3 mjpeg innomakers
+        # still tcp/udp issue?
+        # front_line = "rtspsrc location=rtsp://localhost:8554/front protocols=tcp latency=0 drop-on-latency=true ! rtpjpegdepay ! queue ! jpegdec ! videoconvert ! gtksink name=sink"
+        # ip for OTOPS black router
+        # front_line = "rtspsrc location=rtsp://192.168.0.2:8554/front latency=0 drop-on-latency=true ! rtpjpegdepay ! queue ! jpegdec ! videoconvert ! gtksink name=sink"
+        
+        # Gstreamer connection pipeline strings - This ip for on rover nanobeam/prism network
+        # Large and small streams to fit large and small windows
+        self.front_line = "rtspsrc location=rtsp://192.168.1.31:8554/front latency=0 drop-on-latency=true ! rtph264depay ! queue ! h264parse ! avdec_h264 ! videoconvert ! gtksink name=sink"
+        self.front_line_small = "rtspsrc location=rtsp://192.168.1.31:8554/front latency=0 drop-on-latency=true ! rtph264depay ! queue ! h264parse ! avdec_h264 ! videoconvert ! videoscale ! video/x-raw, width=525, height=295 ! gtksink name=sink"
+        self.left_line_small = "rtspsrc location=rtsp://192.168.1.31:8554/left latency=0 drop-on-latency=true ! rtpjpegdepay ! queue ! jpegdec ! videoconvert ! videoscale ! video/x-raw, width=525, height=295 ! gtksink name=sink2"
+        self.left_line_large = "rtspsrc location=rtsp://192.168.1.31:8554/left latency=0 drop-on-latency=true ! rtpjpegdepay ! queue ! jpegdec ! videoconvert ! gtksink name=sink2"
+        self.right_line_small = "rtspsrc location=rtsp://192.168.1.31:8554/right latency=0 drop-on-latency=true ! rtpjpegdepay ! queue ! jpegdec ! videoconvert ! videoscale ! video/x-raw, width=525, height=295 ! gtksink name=sink3"
+        self.right_line_large = "rtspsrc location=rtsp://192.168.1.31:8554/right latency=0 drop-on-latency=true ! rtpjpegdepay ! queue ! jpegdec ! videoconvert ! gtksink name=sink3"
+        self.back_line_small = "rtspsrc location=rtsp://192.168.1.31:8554/back latency=0 drop-on-latency=true ! rtpjpegdepay ! queue ! jpegdec ! videoconvert ! videoscale ! video/x-raw, width=525, height=295 ! gtksink name=sink4"
+        self.back_line_large = "rtspsrc location=rtsp://192.168.1.31:8554/back latency=0 drop-on-latency=true ! rtpjpegdepay ! queue ! jpegdec ! videoconvert ! gtksink name=sink4"
+
+        self.front_cam_sink = "sink"
+        self.left_cam_sink = "sink2"
+        self.right_cam_sink = "sink3"
+        self.back_cam_sink = "sink4"
+
+        # Uses a callback function to request data from the swap button pressed
+        self.front_cam_grid = LargeCamGrid("Front", self.front_line, self.front_cam_sink, self.swap)
+        self.left_cam_grid = BaseCamGrid("Left", self.left_line_small, self.left_cam_sink)
+        self.right_cam_grid = BaseCamGrid("Right", self.right_line_small, self.right_cam_sink)
+        self.back_cam_grid = BaseCamGrid("Back", self.back_line_small, self.back_cam_sink)
+        
+        # Col, Row, Width, Height
+        root_grid.attach(self.front_cam_grid,0,0,1,3)
+        root_grid.attach(self.left_cam_grid,1,0,1,1)
+        root_grid.attach(self.right_cam_grid,1,1,1,1)
+        root_grid.attach(self.back_cam_grid,1,2,1,1)
+
+    # Swapping Front and target grid by switching pipelines and sinks around
+    def front_swap(self, target_grid:BaseCamGrid,pipeline_str,sink,label_text):
+        self.front_cam_grid.stream_off()
+        target_grid.stream_off()
+
+        # Show big target stream in main grid
+        self.front_cam_grid.pipeline_str = pipeline_str
+        self.front_cam_grid.sink = sink
+        self.front_cam_grid.stream_on()
+        self.front_cam_grid.cam_lbl.set_markup(f"<big>{label_text}</big>")
+
+        # Show front stream in small target grid
+        target_grid.pipeline_str = self.front_line_small
+        target_grid.sink = self.front_cam_sink
+        target_grid.stream_on()
+        target_grid.cam_lbl.set_markup("<big>Front</big>")
+
+    # Reset camera to its originally set stream to undo swaps
+    def cam_reset(self,cam_grid:BaseCamGrid,pipeline_str,sink,label_text):
+        cam_grid.stream_off()
+        cam_grid.pipeline_str = pipeline_str
+        cam_grid.sink = sink
+        cam_grid.stream_on()
+        cam_grid.cam_lbl.set_markup(f"<big>{label_text}</big>")
+
+    # Swap controller, takes swap type and calls swap and reset methods
+    # Swap always between a large front camera and a smaller camera
+    def swap(self, value):
+        if value == "Left" and self.front_cam_grid.left_shown == False:
+            
+            # Need to reset all other cams during swaps as otherwise front would just swap with
+            # everything and all the small cams would be front, don't want to lose track of cams
+            if self.right_cam_grid.pipeline:
+                self.cam_reset(self.right_cam_grid,self.right_line_small, self.right_cam_sink,"Right")
+            if self.back_cam_grid.pipeline:
+                self.cam_reset(self.back_cam_grid,self.back_line_small, self.back_cam_sink,"Back")
+
+            self.front_swap(self.left_cam_grid,self.left_line_large,self.left_cam_sink,value)
+        
+        elif value == "Right" and self.front_cam_grid.right_shown == False:
+            
+            if self.left_cam_grid.pipeline:
+                self.cam_reset(self.left_cam_grid,self.left_line_small,self.left_cam_sink,"Left")
+            if self.back_cam_grid.pipeline:
+                self.cam_reset(self.back_cam_grid,self.back_line_small,self.back_cam_sink,"Back")
+
+            self.front_swap(self.right_cam_grid,self.right_line_large,self.right_cam_sink,value)
+        
+        elif value == "Back" and self.front_cam_grid.back_shown == False:
+            
+            if self.left_cam_grid.pipeline:
+                self.cam_reset(self.left_cam_grid,self.left_line_small,self.left_cam_sink,"Left")
+            if self.right_cam_grid.pipeline:
+                self.cam_reset(self.right_cam_grid,self.right_line_small,self.right_cam_sink,"Right")
+
+            self.front_swap(self.back_cam_grid,self.back_line_large,self.back_cam_sink,value)
+        
+        # Resetting front stream 
+        elif value == "Front" and self.front_cam_grid.front_shown == False:
+            
+            # When front being reset, want everything to also be reset
+            # As either window could be showing front, they need to go back to og view
+            if self.left_cam_grid.pipeline:
+                self.cam_reset(self.left_cam_grid,self.left_line_small,self.left_cam_sink,"Left")
+            if self.right_cam_grid.pipeline:
+                self.cam_reset(self.right_cam_grid,self.right_line_small,self.right_cam_sink,"Right")
+            if self.back_cam_grid.pipeline:
+                self.cam_reset(self.back_cam_grid,self.back_line_small,self.back_cam_sink,"Back")
+
+            self.cam_reset(self.front_cam_grid,self.front_line,self.front_cam_sink,"Front")
+        else:
+            print(f"ERROR: Swap error, streams already swapped")
+
+window = MainWindow()
+window.connect("delete-event", Gtk.main_quit)
+window.show_all()
+Gtk.main()
